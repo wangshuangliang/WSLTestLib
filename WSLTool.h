f4fd2de10e5ab8113bf71240aa200971d3433aee
wsl530syl515 <1397871472@qq.com>
2018-07-30 10:57:20 +0800

    静态库初始化

---
+++ WSLTool.h
@@ -0,0 +1,15 @@
+//
+//  WSLTool.h
+//  WSLTool
+//
+//  Created by wsl 王 on 2018/7/30.
+//  Copyright © 2018年 wsl 王. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WSLTool : NSObject
+
+- (void)printResultWithStr:(NSString *)str;
+
+@end
